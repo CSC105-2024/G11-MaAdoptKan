4d63bea97aee2b05b75b19ae4653b8a6b6f2d4b5
Nitro <tonplu110@gmail.com>
2025-03-22 22:59:55 +0700

    Donation version lastest and put orange logo image to public

--- src/pages/DonationPage.jsx
+++ src/pages/DonationPage.jsx
@@ -1,4 +1,4 @@
-import  React from 'react';
+import React from 'react';
 import './DonationPage.css';
 import Navbar from "../assets/Navbar.jsx";
 
@@ -8,12 +8,15 @@ function DonationPage() {
             <div>
                 <Navbar />
             </div>
-            <div className="min-h-screen flex justify-center items-center">
-                <div className="two-boxes-container flex ">
-                    <div className="box1 w-[500px] h-[60vh] m-[20px] overflow-hidden bg-white rounded-[8px] shadow p-[20px]">
+            <div className="min-h-screen flex justify-center items-center p-4">
+                <div className="two-boxes-container flex flex-col md:flex-row items-center justify-center gap-4">
+                    <div className="box1 sm:w-[355px] md:w-[500px] sm:h-[510px] md:h-[450px] m-[20px] overflow-hidden bg-white rounded-[8px] shadow p-[20px]">
                         <h1 className="text-header">Food & Supplies</h1><br/>
                         <p className="font-medium">Name</p>
-                        <p className="font-medium text-[#E97A28]">Ma Adopt Kan</p><br/>
+                        <p className="font-medium text-[#E97A28] flex items-center">
+                            <img src="public/images/logo-orange.png" alt="logo" className="w-6 h-6 mr-2"/>
+                            Ma Adopt Kan
+                        </p><br/>
                         <p className="font-medium">Tel.</p>
                         <p className="font-medium text-[#E97A28]">0930299202</p><br/>
                         <p className="font-medium">Address</p>
@@ -21,15 +24,19 @@ function DonationPage() {
                             9/168 Prachautit 23 Prachautit Road,<br/> BangMod, Thung Kru, Bangkok, 10140
                         </p>
                     </div>
-                    <div className="box2 w-[500px] h-[60vh] m-[20px] overflow-hidden bg-[#f9f9f9] rounded-[8px] shadow p-[20px]">
-                        <h1 className="text-header">QR Code for Donation</h1>
-                        <img
-                            src="public/images/QRDonate.jpg"
-                            alt="QR Code for Donation"
-                            className="w-44 h-44 mb-3"
-                        />
-                        <p className="font-medium">Bank Account Information</p>
-                        <p className="font-medium text-[#E97A28]">Kbank: 198-8-56887-9</p>
+                    <div className="box2 sm:w-[355px] md:w-[500px] sm:h-[510px] md:h-[450px] m-[20px] overflow-hidden bg-white rounded-[8px] shadow p-[20px] flex flex-col items-start justify-center">
+                        <h1 className="text-header text-left">QR Code for Donation</h1>
+                        <div className="flex justify-center items-center h-[300px] mb-6 w-full">
+                            <img
+                                src="public/images/QRDonate.jpg"
+                                alt="QR Code for Donation"
+                                className="w-full h-auto max-w-[300px] max-h-[300px] object-contain"
+                            />
+                        </div>
+                        <div className="text-center w-full">
+                            <p className="font-medium">Bank Account Information</p>
+                            <p className="font-medium text-[#E97A28]">ttbbank : 245-2-19934-8</p>
+                        </div>
                     </div>
                 </div>
             </div>
