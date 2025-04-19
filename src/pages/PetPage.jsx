01b141c2b39ade2783b9ab546388f858ab74e78a
natebps <natebpss@gmail.com>
2025-04-19 14:36:52 +0700

    Do Zod CreatePost

--- src/pages/PetPage.jsx
+++ src/pages/PetPage.jsx
@@ -1,42 +1,20 @@
-import React from "react";
-import Navbar from "./../assets/Navbar";
-import PopUpCreate from "../popup/PopUpCreate";
+import React, { useState } from "react";
+import Navbar from "../assets/Navbar";
 import CreatePetForm from "../popup/CreatePetForm";
-import { useState } from "react";
 
 export default function PetPage() {
   const [buttonPopup, setButtonPopup] = useState(false);
-  const [type, setType] = useState("");
-  const [gender, setGender] = useState("");
-  const [date, setDate] = useState(null);
 
   return (
     <>
-      <div>
-        <Navbar />
-      </div>
+      <Navbar />
       <div className="flex flex-col items-center w-full max-w-[1440px] mx-auto text-center mt-8 g-24 min-h-screen">
         <div>This is Pet Page!</div>
         <br />
-        <button className="" onClick={() => setButtonPopup(true)}>
-          Create
-        </button>
+        <button onClick={() => setButtonPopup(true)}>Create</button>
 
-        {/* just for example let me try, then i'll move -Nateetan */}
-        <PopUpCreate
-          trigger={buttonPopup}
-          setTrigger={setButtonPopup}
-          className=""
-        >
-          <CreatePetForm
-            type={type}
-            setType={setType}
-            gender={gender}
-            setGender={setGender}
-            date={date}
-            setDate={setDate}
-          />
-        </PopUpCreate>
+        {/* เรียก CreatePetForm ที่รวม popup มาให้แล้ว */}
+        <CreatePetForm trigger={buttonPopup} setTrigger={setButtonPopup} />
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-export {};
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // หรือเปลี่ยนตาม path ที่ใช้จริง
+      "./node_modules/primereact/**/*.{js,jsx,ts,tsx}"
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
