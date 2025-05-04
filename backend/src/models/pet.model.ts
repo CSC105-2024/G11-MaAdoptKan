db6ec0623a701576210b4510657dfc6efad6e41c
natebps <natebpss@gmail.com>
2025-05-04 10:23:51 +0700

    PetAPI

--- backend/src/models/pet.model.ts
+++ backend/src/models/pet.model.ts
@@ -44,6 +44,53 @@ const createPet = async (
   return pet;
 };
 
+const editPet = async (
+  id: number,
+  name: string,
+  type: $Enums.Type,
+  gender: $Enums.Gender,
+  pictureUrl: string,
+  phoneNumber: string,
+  address: string,
+  userId: number,
+  color?: string,
+  dateOfBirth?: Date,
+  ageYear?: number,
+  ageMonth?: number,
+  breed?: string,
+  vaccineUrl?: string,
+  vaccines?: Record<"vaccine", string>[],
+) => {
+  const pet = await db.pet.update({
+    where:{
+      id: id
+    },
+    data: {
+      name,
+      type,
+      pictureUrl,
+      gender: gender,
+      color,
+      dateOfBirth,
+      ageYear,
+      ageMonth,
+      breed,
+      phoneNumber,
+      address,
+      vacineUrl: vaccineUrl,
+      userId,
+      ...(vaccines && {
+        Vaccine: {
+          createMany: {
+            data: vaccines,
+          }
+        }
+      }),
+    },
+  });
+  return pet;
+};
+
 const getPet = async (id: number) => {
   const pet = await db.pet.findUnique({
     where: {
@@ -58,4 +105,13 @@ const getAllPet = async () => {
   return pet;
 };
 
-export { createPet, getPet, getAllPet };
+const deletePet = async (id: number) => {
+  const pet = await db.pet.delete({
+    where:{
+      id:id
+    }
+  })
+  return pet;
+}
+
+export { createPet, getPet, getAllPet, editPet, deletePet };
